b52545d35c95270365095416a472e9824278edf9
Kary Ma <karyma101@hotmail.com>
2020-09-11 03:49:45 -0700

    Connected Components to App Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import Nav from './components/Nav'
+import Header from './components/Header'
+import Projects from './components/Projects'
+import About from './components/About'
+import Footer from './components/Footer'
+
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <Nav />
+            <Header />
+            <Projects />
+            <About />
+            <Footer />
+        </>
     )
 }
 
