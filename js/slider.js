b471b6883473feda334eb6604051e8bcdb9d0da8
Kary Ma <karyma101@hotmail.com>
2020-09-10 17:33:12 -0700

    fresh start

--- js/slider.js
+++
@@ -1,6 +0,0 @@
-function toggleDarkmode(){
-  document.body.classList.toggle("dark-mode");
-  document.querySelector(".about").classList.toggle("dark-mode");
-  document.querySelector(".nav").classList.toggle("dark-header");
-  document.querySelector(".footer").classList.toggle("dark-header");
-}
